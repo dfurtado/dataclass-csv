ee8e404210a976533dc66e5dd21b8b6a769e5a2d
Daniel Furtado <daniel@dfurtado.com>
2018-12-16 16:33:03 +0100

    Argument renaming

--- dataclass_csv/exceptions.py
+++ dataclass_csv/exceptions.py
@@ -1,7 +1,7 @@
 class CsvValueError(Exception):
-    def __init__(self, msg, line_number):
-        self.msg = msg
+    def __init__(self, error, line_number):
+        self.error = error
         self.line_number = line_number
 
     def __str__(self):
-        return f'{self.msg} [CSV Line number: {self.line_number}]'
+        return f'{self.error} [CSV Line number: {self.line_number}]'
